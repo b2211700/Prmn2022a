e9e9a91a893fec3ed1358dff0935bd22d4c4b9a4
b2211700 <b2211700@photon.chitose.ac.jp>
2022-11-02 23:14:33 +0900

    課題1-2

---
+++ prmn2022a/src/lecture01/kadai12.java
@@ -0,0 +1,9 @@
+package lecture01;
+
+public class kadai12 {
+    public static void main(String[] args) {
+        int studentNumber = 2211700;
+        System.out.println("B" + studentNumber + " Kako Toriyabe");
+
+    }
+}
