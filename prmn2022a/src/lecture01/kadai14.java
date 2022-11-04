eb8032e7683cac0c23921b4b35f7a3ba792c7e30
b2211700 <b2211700@photon.chitose.ac.jp>
2022-11-04 14:57:49 +0900

    課題1-4

---
+++ prmn2022a/src/lecture01/kadai14.java
@@ -0,0 +1,16 @@
+package lecture01;
+
+public class kadai14 {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+
+        for (int i = 0; i < 100; i++) {
+            array[i] = i + 1;
+        }
+        int sum = 0;
+        for (int j = 0; j < 100; j = j + 2) {
+            sum = sum + array[j];
+        }
+        System.out.println(sum);
+    }
+}
