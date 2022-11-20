deec43b1773b8eeb92ab1ff33c287fd811b9be62
b2211700 <b2211700@photon.chitose.ac.jp>
2022-11-20 23:08:19 +0900

    課題3-3

--- prmn2022a/src/lecture03/Exercise3_3.java
+++ prmn2022a/src/lecture03/Exercise3_3.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class Exercise3_3 {
     public static void main(String[] args) {
-
         System.out.println("1つ目の整数を入力してください:");
         Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt();
-        scanner.next();
+        //一行読み取ってから、文字列を整数型に変換
+        String num1 = scanner.nextLine();
+        int number1 = Integer.parseInt(num1);
 
         System.out.println("2つ目の整数を入力してください:");
-        int num2 = scanner.nextInt();
-        scanner.next();
+        String num2 = scanner.nextLine();
+        int number2 = Integer.parseInt(num2);
+        int sum = number1 + number2;
 
-        int sum = num1 + num2;
-        System.out.println(num1 + " + " + num2 + " = " + sum);
-        }
-
+        System.out.println(number1 + " + " + number2 + " = " + sum);
     }
+}
+
+
