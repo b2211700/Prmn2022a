a07792330a60d61d1a808e2f386f8ba9ea534abc
b2211700 <b2211700@photon.chitose.ac.jp>
2022-11-25 17:07:16 +0900

    課題5-1

---
+++ prmn2022a/src/lecture05/Exercise5_1.java
@@ -0,0 +1,8 @@
+package lecture05;
+
+public class Exercise5_1 {
+    public static void main(String[] args) {
+        Insert insert = new Insert();
+        insert.move();
+    }
+}
---
+++ prmn2022a/src/lecture05/Insert.java
@@ -0,0 +1,8 @@
+package lecture05;
+
+public class Insert {
+
+    public void move(){
+        System.out.println("歩いたよ");
+    }
+}
