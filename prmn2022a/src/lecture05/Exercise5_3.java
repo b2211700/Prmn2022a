164527726ac7e3ea3080ab392b8fce9e2aafbd8e
b2211700 <b2211700@photon.chitose.ac.jp>
2022-11-25 17:27:36 +0900

    課題5-3

---
+++ prmn2022a/src/lecture05/Exercise5_3.java
@@ -0,0 +1,11 @@
+package lecture05;
+
+public class Exercise5_3 {
+
+    public static void main(String[] args) {
+        Butterfly butterfly = new Butterfly();
+        Insert butterfly1 = butterfly;
+
+        butterfly1.move();
+    }
+}
