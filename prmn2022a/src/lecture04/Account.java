2d8e1f30194d0dadbe8cdcdc3a819ec4b46780e3
b2211700 <b2211700@photon.chitose.ac.jp>
2022-11-25 16:58:40 +0900

    課題4-2

---
+++ prmn2022a/src/lecture04/Account.java
@@ -0,0 +1,28 @@
+package lecture04;
+
+public class Account {
+    private String name;
+    private String number;
+    private long balance;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+    public Account(String name, String number){
+        this.name = name;
+        this.number = number;
+        this.balance =0;
+    }
+}
