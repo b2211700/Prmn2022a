0aed32da32394f5548845bc00fec3324c9cc9722
b2211700 <b2211700@photon.chitose.ac.jp>
2022-11-26 16:23:30 +0900

    課題5-3

--- prmn2022a/src/lecture05/Exercise5_5.java
+++ prmn2022a/src/lecture05/Exercise5_5.java
@@ -5,7 +5,7 @@ public class Exercise5_5 {
     public static void main(String[] args) {
 
         Locust locust = new Locust();
-        //Insect locust1 = locust;
+        Insect locust1 = locust;
 
 
 
