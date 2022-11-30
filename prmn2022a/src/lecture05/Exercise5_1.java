2c8acb881e3793261a7ffff2fead2dbdd2591798
b2211700 <b2211700@photon.chitose.ac.jp>
2022-11-30 09:34:54 +0900

    課題5-1

--- prmn2022a/src/lecture05/Exercise5_1.java
+++ prmn2022a/src/lecture05/Exercise5_1.java
@@ -3,7 +3,7 @@ package lecture05;
 public class Exercise5_1 {
     public static void main(String[] args) {
 
-        Insect insect = new Insect("歩いたよ");
+        Insect insect = new Insect();
         insect.move();
 
     }
--- prmn2022a/src/lecture05/Insect.java
+++ prmn2022a/src/lecture05/Insect.java
@@ -1,16 +1,9 @@
 package lecture05;
 
 public class Insect {
-
-    protected String dou;
-        public Insect(){
-        }
-        public Insect(String dou) {
-            this.dou = dou;
-        }
-        public void move(){
-            System.out.println(dou);
-        }
-
+    public void move(){
+        System.out.println("歩いたよ");
     }
+
+}
 
